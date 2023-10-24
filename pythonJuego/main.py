413d4587f40167c2153e93c4a5c066715c26d9b3
CesaRivero <cesar_tomas_rivero@hotmail.com>
2023-10-24 19:35:05 -0300

    feat: README.md

--- pythonJuego/main.py
+++ pythonJuego/main.py
@@ -6,7 +6,10 @@ from clases.juego import Juego
 from clases.variables import ancho, alto
 
 def main():
-
+    # conexion = Conexion('base_de_datos/mibase.db')
+    # conexion.crearTabla()
+    # conexion.agregar(self.player_name, self.score)
+    #  ver donde iria esto. esta version no guarda en bdd.
     pygame.init()
     pygame.mixer.init()
     ventana = pygame.display.set_mode((ancho,alto))
